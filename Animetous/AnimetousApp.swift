08d1534ad66079076e489ef0b58a79cb88f49777
nsbblood <enesarikan4444@gmail.com>
2024-12-02 21:32:25 +0300

    Initial Commit

---
+++ Animetous/AnimetousApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimetousApp.swift
+//  Animetous
+//
+//  Created by enes arikan on 2.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimetousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimetousTests/AnimetousTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimetousTests.swift
+//  AnimetousTests
+//
+//  Created by enes arikan on 2.12.2024.
+//
+
+import Testing
+@testable import Animetous
+
+struct AnimetousTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
